de9f41acaa736d93bdb8fc6266aaf895d5169ff9
Ashwin <ashwinbkrish@gmail.com>
2022-11-02 12:04:47 +0530

    initial setup of tailwind css and project complete

---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Ashwin&apos;s Portfolio</title>
+       
+      </Head>
+
+      <h1 className='p-10 text-red-500'>Lezzz build a portfolio</h1>
+
+
+      {/* Header */}
+
+
+
+      {/* HERO */}
+
+
+      {/* About */}
+
+
+      {/* Experience */}
+
+
+      {/* Skills */}
+
+
+      {/* Projects */}
+
+
+      {/* Contact me */}
+    </div>
+    
+  )
+}
