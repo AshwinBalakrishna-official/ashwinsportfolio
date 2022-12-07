818f42e45b879f95aa9c0a8afa0681f7769b283e
Ashwin <ashwinbkrish@gmail.com>
2022-12-07 10:48:33 +0530

    completed contact section content layout yet to be finalised

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SectionHeader from './SectionHeader'
-import { PhoneIcon } from '@heroicons/react/24/solid'
+import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/solid'
 
 type Props = {}
 
@@ -15,13 +15,33 @@ const ContactMe = (props: Props) => {
                 <span className='text-xl font-semibold underline decoration-[#F7AB0A]'> Lets Talk</span>
             </h4>
 
-
+            {/* Mobile Number */}
             <div className='mt-3 flex items-center space-x-5 justify-center'>
-                <PhoneIcon className='w-5'/>
-                <p>+91 9791091511</p>
-
+                <PhoneIcon className='text-[#F7AB0A]/80 w-7 h-7 animate-pulse'/>
+                <p className='text-2xl'>+91 9791091511</p>
             </div>
+
+            {/* Email */}
+            <div className='mt-3 flex items-center space-x-5 justify-center'>
+                <EnvelopeIcon className='text-[#F7AB0A]/80 w-7 h-7 animate-pulse'/>
+                <p className='text-2xl'>ashwinbkrish@gmail.com</p>
+            </div>
+
+            <div className='flex flex-row '>
+            
+                <div >
+                   <button className='w-[200px]` bg-[#F7AB0A]/50 '>Make a Call</button>
+                </div> 
+                
+                <div>
+                   <button className='w-full bg-[#F7AB0A]/50 '>Make a Call</button>
+                </div> 
+                {/* <button className="w-[50%] ">Send a Mail</button> */}
+
+            </div>          
         </div>
+
+
     
       
     </div>
